8a1133dea9451f7a1a2e4aa31b4d9f1b1bf3b266
Zerth Monk <26793601+zerthmonk@users.noreply.github.com>
2020-11-23 13:31:18 +0200

    .env for tester environment

--- app.py
+++ app.py
@@ -1,16 +1,20 @@
-import os
 import sys
-import json
-import socket
 
-from skabenclient.config import SystemConfig
-from skabenclient.helpers import get_mac
-from skabenclient.main import start_app
+arguments = dict(enumerate(sys.argv[1:]))  # exclude script name
+if len(arguments) != 1 or arguments.get(0) in ('help', '-h', '--help'):
+    print('\n  look up for tester environment in .env file\n')
+    exit()
+else:
+    import os
+    import json
 
-from dotenv import load_dotenv
+    from skabenclient.config import SystemConfig
+    from skabenclient.main import start_app
 
-from device import TesterDevice
-from config import TesterConfig
+    from dotenv import load_dotenv
+
+    from device import TesterDevice
+    from config import TesterConfig
 
 root = os.path.abspath(os.path.dirname(__file__))
 
@@ -20,45 +24,28 @@ dev_config_path = os.path.join(root, 'conf', 'device.yml')
 log_path = os.path.join(root, 'local.log')
 
 
-def get_interface_name():
-    return socket.if_nameindex()
-
-
 if __name__ == "__main__":
-    # get arguments
-    arguments = dict(enumerate(sys.argv[1:]))  # exclude script name
-    if len(arguments) < 3 or arguments.get(0) in ('help', '-h', '--help'):
-        print('\nUsage: python app.py <topic> <broker_ip> <interface running on> <-d>\n')
-        exit()
-
-    web = True
-    if '-d' in arguments.values():
-        web = None
-
-    # assign default values
     load_dotenv(dotenv_path='.env')
-    print(get_interface_name())
 
-    test_conf = {
-                'topic': arguments.get(0, 'ask'),
-                'broker_ip': arguments.get(1, '192.168.0.200'),
-                'iface': arguments.get(2, 'eth0'),
-                'delay': arguments.get(3, 0),
+    TESTER_CONF = {
+                'topic': os.getenv('TOPIC', 'ask'),
+                'broker_ip': os.getenv('BROKER_IP', '192.168.0.200'),
+                'iface': os.getenv('INTERFACE', 'eth0'),
+                'web_gui': os.getenv("WEB_GUI", True),
+                'delay': os.getenv('DELAY', 0),
                 'username': os.environ.get('MQTT_USERNAME'),
                 'password': os.environ.get('MQTT_PASSWORD'),
-                }
+    }
 
     with open(sys_config_path, 'w') as fh:
-        fh.write(json.dumps(test_conf))
+        fh.write(json.dumps(TESTER_CONF))
 
-    # setting system configuration and logger
     app_config = SystemConfig(sys_config_path, root=root)
     app_config.logger(file_path=log_path)
-    # inject arguments into system configuration
-    app_config.update(test_conf)
+    app_config.update(TESTER_CONF)
+
     device_config = TesterConfig(dev_config_path)
-    # instantiating device
-    device = TesterDevice(app_config, device_config, web=web)
+    device = TesterDevice(app_config, device_config, web=TESTER_CONF.get('web_gui'))
 
     start_app(app_config=app_config,
               device=device)
